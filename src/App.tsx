f1e6ad69994b8b8d7bded7311649ae5c897284a8
Andriy Bobchuk <andriybobchuk@gmail.com>
2024-03-11 21:30:25 +0100

    done fixing shit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/Navbar';
+import HeroSection from './components/Hero';
 
 export default function App() {
   return (
     <div>
       <NavBar />
+      <HeroSection />
     </div>
   );
 }
