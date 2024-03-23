b4709597b96db3757b9c55eb42634d3def861219
Andriy Bobchuk <andriybobchuk@gmail.com>
2024-03-23 12:00:31 +0100

    Fixed reports, added language detection

--- src/components/ReportsPage.tsx
+++ src/components/ReportsPage.tsx
@@ -33,6 +33,13 @@ const ReportsPage: React.FC = () => {
       ...doc.data(),
       uploadedDate: new Date(doc.data().uploadedDate.seconds * 1000).toLocaleDateString() // Adjust depending on your date format
     })) as FileMetadata[];
+
+    filesList.sort((a, b) => {
+      const dateA = new Date(a.uploadedDate);
+      const dateB = new Date(b.uploadedDate);
+      return dateB.getTime() - dateA.getTime(); // Sort in descending order; use dateA - dateB for ascending
+    });
+    
     setFiles(filesList);
     setLoading(false);
   };
@@ -78,26 +85,26 @@ const ReportsPage: React.FC = () => {
 
   return (
     <div className='bg-gray-100'>
-      <div className="container h-screen max-w-6xl mx-auto pt-28">
-        <h1 className="text-3xl font-bold mb-8">Reports</h1>
+      <div className="p-3 h-auto pb-60 max-w-6xl mx-auto pt-28">
+        <h1 className="text-3xl font-bold mb-8">Звіти</h1>
         {/* <p className="text-lg mb-8">On this page you can view and download all the reports of our company</p> */}
         {isAdminUser(currentUser) && (
-          <div className="flex items-center justify-between mb-4 bg-white rounded-2xl p-4 px-6">
-            <input type="file" onChange={handleFileChange} className="mr-2" />
-            <input type="text" value={customName} onChange={(e) => setCustomName(e.target.value)} placeholder="Enter a meaningful name" className="mr-2 p-2 pl-4 bg-gray-100 rounded-md" />
-            <button onClick={uploadFile} className="px-4 py-2 bg-purple text-white rounded hover:bg-purple transition duration-300">Upload New Report</button>
+          <div className="flex flex-col md:flex-row items-center justify-between space-y-4 sm:space-y-0 mb-4 bg-white rounded-2xl p-4">
+            <input type="file" onChange={handleFileChange} className="ml-10" />
+            <input type="text" value={customName} onChange={(e) => setCustomName(e.target.value)} placeholder="Назва звіту" className="mr-2 p-2 pl-4 bg-gray-100 rounded-md" />
+            <button onClick={uploadFile} className="px-4 py-2 bg-purple text-white rounded hover:bg-purple transition duration-300">Опублікувати звіт</button>
           </div>
         )}
 
-        <div className="space-y-4 bg-white rounded-2xl p-4 px-6">
+        <div className="space-y-8 sm:space-y-4 bg-white rounded-2xl p-4 px-6">
           {files.map((file) => (
-            <div key={file.id} className="flex items-center justify-between">
+            <div key={file.id} className="flex flex-col md:flex-row items-center justify-between space-y-1 sm:space-y-0">
               <span className="text-lg">{file.customName}</span>
               <span className="text-sm text-gray-600">{file.uploadedDate}</span>
               <div className="space-x-2">
-                <a href={file.url} download={file.customName} className="px-4 py-2 bg-gray-100 text-gray-600 rounded hover:bg-purple hover:text-white transition duration-300">Download</a>
+                <a href={file.url} download={file.customName} className="px-4 py-2 bg-gray-100 text-gray-600 rounded hover:bg-purple hover:text-white transition duration-300">Скачати</a>
                 {isAdminUser(currentUser) && (
-                  <button onClick={() => deleteFile(file.id, `reports/${file.name}`)} className="px-4 py-1 bg-red-500 text-white rounded hover:bg-red-700 transition duration-300">Delete</button>
+                  <button onClick={() => deleteFile(file.id, `reports/${file.name}`)} className="px-4 py-1 bg-red-500 text-white rounded hover:bg-red-700 transition duration-300">Видалити</button>
                 )}
               </div>
             </div>
